49b93fa18a696299c3231a31244ec68290df08ab
HimanshuIIITH <himanshu.kumar@students.iiit.ac.in>
2020-10-15 22:26:36 +0530

    node arrival key redistribution

---
+++ client/headerfile.h
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream>
+#include <time.h> 
+#include<sys/stat.h>
+#include <sys/types.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/sendfile.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <bits/stdc++.h> 
+#include<dirent.h>
+#include <sys/wait.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <openssl/sha.h> 
+using namespace std;
